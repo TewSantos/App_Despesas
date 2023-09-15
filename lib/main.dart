ee99dc5588b73c203f2ab493e678c43a69b0d1a0
TewSantos <mateustews@gmail.com>
2023-09-14 21:42:29 -0300

    Criação do documento Transaction Na Pasta Models

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+import 'package:expenses/models/Transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
 
 
 class ExpensesApp extends StatelessWidget {
+ final _transactions = [
+  Transaction(
+    id: 't1', 
+    title: 'Novo Tênis de Corrida', 
+    value: 310.76, 
+    date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2', 
+      title: 'Conta De Luz', 
+      value: 211.30, 
+      date: DateTime.now(),
+      ),
+ ];
   @override
   Widget build (BuildContext context) {
     return MaterialApp(
@@ -20,9 +35,10 @@ class MyHomePage extends StatelessWidget {
       title: Text ('Despesas Pessoais'),
      ),
      body: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Container(
-          width: double.infinity,
         child: Card(
           color: Colors.blue,
           child: Text ('Gráfico'),
