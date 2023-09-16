bc647a9e833f319b049e78a6c36e714b054dac51
TewSantos <mateustews@gmail.com>
2023-09-15 22:48:39 -0300

    Atualizações nos campos: Valor, Titulo e data da compra.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,16 @@ main() => runApp(ExpensesApp());
 
 
 class ExpensesApp extends StatelessWidget {
- final _transactions = [
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      home:MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
   Transaction(
     id: 't1', 
     title: 'Novo Tênis de Corrida', 
@@ -20,15 +29,6 @@ class ExpensesApp extends StatelessWidget {
       ),
  ];
   @override
-  Widget build (BuildContext context) {
-    return MaterialApp(
-      home:MyHomePage()
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -37,16 +37,62 @@ class MyHomePage extends StatelessWidget {
      body: Column(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Container(
+      children: [
+        const SizedBox(
         child: Card(
           color: Colors.blue,
           child: Text ('Gráfico'),
           elevation: 5,
         ),
        ),
-        Card( 
-          child: Text ('Lista de Transações'),
+        Column(
+          children: _transactions.map((tr){
+            return Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      tr.value.toString(),
+                      style: const TextStyle (
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:[
+                      Text(
+                        tr.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                      )
+                      ),
+                      Text(tr.date.toString(),
+                      style: const TextStyle(
+                          color: Colors.grey
+                      )
+                      ),
+                      
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
         )
       ]
      ),
