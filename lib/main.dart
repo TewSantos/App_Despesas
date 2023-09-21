669b48d1a7dbf415ca1f5a64fd42a032d5d51aaf
TewSantos <mateustews@gmail.com>
2023-09-21 00:00:05 -0300

    Capturando dados do Fomulário

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,10 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
   Transaction(
     id: 't1', 
@@ -103,11 +107,13 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children:[
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(
                       labelText: 'Título',
                     )
                   ),
                   TextField(
+                    controller: valueController,
                     decoration: InputDecoration(
                       labelText: 'Valor (R\$)',
                     )
@@ -122,7 +128,10 @@ class MyHomePage extends StatelessWidget {
                             color: Colors.purple,
                           ),
                           ),
-                        onPressed: () {},
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
                       ),
                     ],
                   )
